8bb74eddf91880dc578f0d5ff24d48b0844a3a40
Aditya Kumar <adityarajsingh2048@gmail.com>
2025-08-07 17:02:57 +0530

    DAY 8 COMPLETED

---
+++ DAY_9/bruteFrc.cpp
@@ -0,0 +1,24 @@
+// Maximum subarray sum  _ Brute Force Approach
+
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+int main(){
+    int n = 7; 
+    int arr[7] = {3,-4,5,4,-1,7,-8};
+
+    int maxSum = INT8_MIN;
+
+    for(int st = 0 ; st<n ; st++){
+        int currSum = 0 ; 
+        for(int end = st ; end<n ; end++){
+            currSum += arr[end];
+            maxSum = max(currSum , maxSum);
+        }
+
+    }
+    cout<<"Max Subarray sum = " << maxSum << endl; 
+
+    return 0 ; 
+}
